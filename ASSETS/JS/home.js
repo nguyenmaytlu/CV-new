b2cda874d9245a9ded2391ed8017d8ed2d18ec31
nguyenmay250702 <nguyenmay308@gmail.com>
2022-03-19 13:55:38 +0700

    bổ sung

---
+++ ASSETS/JS/home.js
@@ -0,0 +1,15 @@
+
+// CONTACT
+
+var fullNameContact = document.querySelector(".content-Contact .form-element:nth-child(1) input")
+var emailAddressContact = document.querySelector(".content-Contact .form-element:nth-child(2) input")
+var messageContact = document.querySelector(".content-Contact .form-element:nth-child(3) input")
+var buttonContact = document.querySelector(".content-Contact button")
+
+// fullNameContact.style.borderBottomColor = "red"
+
+function error(input){
+    input.style.borderBottomColor = "red"
+}
+
+fullNameContact.addEventListener("")
